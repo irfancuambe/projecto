16ac0213658a1c5fae54630daddcfae76e8dfcbc
IrfanCuambe <irfanbaptistacuambe@gmail.com>
2023-03-22 18:29:26 +0200

    Initial commit

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+include_once("header.php");  ?>
+
+<div id="layoutSidenav_content">
+                <main>
+                   <h1>ed</h1>
+
+            </main>
+            
+            <h1>Irfan</h1>
+</div>
+<?php
+include_once("footer.php");
+  ?>
+
+ 
